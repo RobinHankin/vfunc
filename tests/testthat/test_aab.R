2839b2a9a355b7bff00dd46238cd319045ebbaf1
RobinHankin <hankin.robin@gmail.com>
2025-02-04 09:58:44 +0000

    coverage

--- tests/testthat/test_aab.R
+++ tests/testthat/test_aab.R
@@ -1,82 +1,27 @@
-test_that("Test suite aab",{
+test_that("Test suite aab",{  # mostly testing Sin, etc
 
     expect_close <- function(x,y){expect_true(all(abs(x-y) < 1e-8))}
 
-    f <- as.vf(function(x){x^2})
-    g <- as.vf(function(x){x^2 + 1})
 
 
-    ## test vf.vf.eq() et seq:
-
-    expect_true ((f == f)(5))
-    expect_true ((g == g)(5))
-    expect_false((f == g)(5))
-
-    expect_false((f != f)(5))
-    expect_false((g != g)(5))
-    expect_true ((f != g)(5))
-    expect_true ((g != f)(5))
-
-    expect_false((f >  g)(5))
-    expect_true ((g >  f)(5))
-
-    expect_false((f >= g)(5))
-    expect_true ((g >= f)(5))
-
-    expect_true ((f <  g)(5))
-    expect_false((g <  f)(5))
-
-    expect_true ((f <= g)(5))
-    expect_false((g <= f)(5))
-
-
-    ##  now .vf.ANY.eq() et seq
+    testvf <- function(vf1,vf2,x){
+        usevf <- c((vf1 + vf2)(x), (vf1(vf2))(x), vf2(vf1)(x), vf2(vf2(x)),(vf2 + vf2)(x))
+        usebr <- c(vf1(x) + vf2(x),vf1(vf2(x)), vf2(vf1(x)), vf2(vf2(x)), vf2(x) + vf2(x))
+        expect_close(usevf,usebr)
+    }
     
-    expect_true ((f == 25)(5))
-    expect_false((f == 26)(5))
+    l1 <-list(as.vf(function(x){x^2}), as.vf(function(x){x^2 + 1}))
 
-    expect_true ((f != 24)(5))
-    expect_false((f != 25)(5))
+    l2 <- list(Abs, Sign, Sqrt, Ceiling, Floor, Trunc, Cummax, Cummin,
+               Cumprod, Cumsum, Log, Log10, Log2, Log1p, Acos, Acosh,
+               Asin, Asinh, Atan, Atanh, Exp, Expm1, Cos, Cosh, Cospi,
+               Sin, Sinh, Sinpi, Tan, Tanh, Tanpi, Gamma, Lgamma,
+               Digamma, Trigamma)
 
-    expect_true ((f >  24)(5))
-    expect_false((f >  25)(5))
-    expect_false((f >  26)(5))
+    for(i in seq_along(l1)){
+        for(j in seq_along(l2)){
+            testvf(l1[[i]], l2[[i]],1.1)
+        }
+    }
 
-    expect_true ((f >= 24)(5))
-    expect_true ((f >= 25)(5))
-    expect_false((f >= 26)(5))
-
-    expect_false((f <  24)(5))
-    expect_false((f <  25)(5))
-    expect_true ((f <  26)(5))
-
-    expect_false((f <= 24)(5))
-    expect_true ((f <= 25)(5))
-    expect_true ((f <= 26)(5))
-
-
-    ##  now .ANY.vf.eq() et seq
-    
-    expect_true ((25 == f)(5))
-    expect_false((26 == f)(5))
-
-    expect_true ((24 != f)(5))
-    expect_false((25 != f)(5))
-
-    expect_false((24 >  f)(5))
-    expect_false((25 >  f)(5))
-    expect_true ((26 >  f)(5))
-
-    expect_false((24 >= f)(5))
-    expect_true ((25 >= f)(5))
-    expect_true ((26 >= f)(5))
-
-    expect_true ((24 <  f)(5))
-    expect_false((25 <  f)(5))
-    expect_false((26 <  f)(5))
-
-    expect_true ((24 <= f)(5))
-    expect_true ((25 <= f)(5))
-    expect_false((26 <= f)(5))
-    
 })
