bb39f08e6c5cb6008573e7aca731530e79a22120
RobinHankin <hankin.robin@gmail.com>
2025-07-23 11:14:24 +0100

    more coverage

--- tests/testthat/test_aae.R
+++ tests/testthat/test_aae.R
@@ -1,4 +1,4 @@
-test_that("Test suite aae",{  # testing pow()
+test_that("Test suite aae",{  # testing pow() and "^"
 
     expect_close <- function(x,y){expect_true(all(abs(x-y) < 1e-8))}
 
@@ -18,5 +18,9 @@ test_that("Test suite aae",{  # testing pow()
     test_f(Sin, 3.2)
     test_f(f(Sin),0.3)
 
-    
+
+    expect_true((f^g)(1) == f(1)^g(1))
+    expect_true((g^f)(1) == g(1)^f(1))
+    expect_true((2^f)(3) == 2^f(3))
+    expect_true((+f)(3) == f(3))
 })
